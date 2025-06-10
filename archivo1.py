5d2a3cd5644725b6c084991603122339628929fa
LuisCReyes <lcreyes@uamv.edu.ni>
2025-06-10 07:48:12 -0600

    archivo1

--- archivo1.py
+++ archivo1.py
@@ -1,1 +1,8 @@
-mi_archivo = open ("C:\\Users\\lcrf_\\OneDrive\\Desktop\\UAM\\Introducción a la programación\\Repositorios de git\\IntroProg2-S12\\archivos txt\\datos.txt", "r")
+try: 
+    mi_ruta = "C:\\Users\\lcrf_\\OneDrive\\Desktop\\UAM\\Introducción a la programación\\Repositorios de git\\IntroProg2-S12\\archivostxt\\"
+    mi_archivo = open (mi_ruta + "datos.txt", "r")
+    contenido = mi_archivo.read()
+    print(contenido)
+    mi_archivo.close
+except FileNotFoundError:
+    print("El archivo no se encuentra en la ruta especificada.")
